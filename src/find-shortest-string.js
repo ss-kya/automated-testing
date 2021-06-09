71f3bd393ffffa74a27f87c3f8f5d04a6eb8b618
Sena Sarkaya <sena_sarkaya@hotmail.com>
2021-06-09 15:36:03 +0100

    final

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -19,9 +19,9 @@ function findShortestString(stringArr) {
   return shortestString;
 }
 
-console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
-console.log(findShortestString(["why", "hello", "to", "you"]), "to");
-console.log(findShortestString(["brave", "dance"]), "brave");
+// console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
+// console.log(findShortestString(["why", "hello", "to", "you"]), "to");
+// console.log(findShortestString(["brave", "dance"]), "brave");
 
 // export findShortestString
 module.exports = findShortestString;
